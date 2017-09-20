d692cc1f9e73a67b2038ed9ecc4e4f292b0017ee
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-20 15:04:56 +0800

    added home function with return render, base template and context| how to render a template

--- src/Project/settings/__init__.py
+++ src/Project/settings/__init__.py
@@ -4,9 +4,9 @@
 
 from .base import *
 
-from .production import *
+# from .production import *
 
-try:
-   from .local import *
-except:
-   pass
+# try:
+#    from .local import *
+# except:
+#    pass
--- src/restaurant/views.py
+++ src/restaurant/views.py
@@ -1,21 +1,25 @@
+import random
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 #function based view
 
+# def home_old(request):
+#  	#f strings
+#  	html_var = 'Louis gwapo'
+#  	html_ = f"""<!DOCTYPE html>
+#  	<html lang=en>
+#  	<head>
+#  	</head>
+#  	<body>
+#  	<h1>Hello World!</h1>
+#  	<p>This is {html_var} coming through</p>
+#  	</body>
+#  	</html>
+#  	"""
+#  	return HttpResponse(html_)
+
 def home(request):
-	#f strings
-	html_var = 'Louis gwapo'
-	html_ = f"""<!DOCTYPE html>
-	<html lang=en>
-	<head>
-	</head>
-	<body>
-	<h1>Hello World!</h1>
-	<p>This is {html_var} coming through</p>
-	</body>
-	</html>
-	"""
-	return HttpResponse(html_)
-	#return render(request,"home.html",{})#response
+	num = random.randint(0, 1000000)
+	return render(request,"base.html",{"html_var": True, "num" : num})#response
